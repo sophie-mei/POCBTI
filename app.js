a9dbf6bf810a5eed35bd1d69d477662782b537ec
sophie-mei <sophie-mei@live.fr>
2021-12-16 12:13:56 +0100

    initail commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//ROUTES
+
+app.get('/consentement_import', (req, res) => {
+    res.send();
+});
+
+app.get('/data_import', (req, res) => {
+    res.send();
+});
+
+
+app.listen(3000);
+    
